42d408b934d8a8e612319bcaefdbe374e6ac6e37
Klaus Alfert <klaus.alfert@googlemail.com>
2012-05-23 12:21:32 +0200

    Konfiguration mit dem Postgresql-Rezept kombiniert

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,3 +15,32 @@ package "pgpool2" do
 end
 
 
+template "/etc/pgpool2/pgpool.conf" do
+	source "pgpool.conf.erb"
+	owner "root"
+	group "root"
+	mode "0644"
+	notifies :restart, "service[pgpool2]"
+end
+
+template "/etc/pgpool2/pcp.conf" do
+	source "pcp.conf.erb"
+	owner "root"
+	group "root"
+	mode "0644"
+	notifies :restart, "service[pgpool2]"
+end
+
+template "/etc/pgpool2/pool_hba.conf" do
+	source "pool_hba.conf.erb"
+	owner "root"
+	group "root"
+	mode "0644"
+	notifies :restart, "service[pgpool2]"
+end
+
+
+service "pgpool2" do
+	supports :status => true, :restart => true, :reload => true
+	action [ :enable, :start ]
+end
